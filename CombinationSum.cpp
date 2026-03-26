8c92c01de66631d7966c608c7b594850e2ebdda1
Laxmi Gupta <laxmigupta188777@gmail.com>
2026-03-26 20:43:31 +0530

    Create CombinationSum.cpp
    Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
    
    The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
    
    The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
    
     
    
    Example 1:
    
    Input: candidates = [2,3,6,7], target = 7
    Output: [[2,2,3],[7]]
    Explanation:
    2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
    7 is a candidate, and 7 = 7.
    These are the only two combinations.

---
+++ CombinationSum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void solve(int idx, vector<int>& candidates, int target,
+               vector<int>& temp, vector<vector<int>>& ans) {
+        
+        // Base case
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (idx == candidates.size() || target < 0) {
+            return;
+        }
+
+        // 🔹 Take current element
+        if (candidates[idx] <= target) {
+            temp.push_back(candidates[idx]);
+            solve(idx, candidates, target - candidates[idx], temp, ans);
+            temp.pop_back(); // backtrack
+        }
+
+        // 🔹 Skip current element
+        solve(idx + 1, candidates, target, temp, ans);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        solve(0, candidates, target, temp, ans);
+        return ans;
+    }
+};
