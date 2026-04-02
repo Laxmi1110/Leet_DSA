64f9c6a26ab40932899f9c2f12d5875c210b4753
Laxmi Gupta <laxmigupta188777@gmail.com>
2026-04-02 23:19:28 +0530

    Implement myPow function for power calculation
    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
    
     
    
    Example 1:
    
    Input: x = 2.00000, n = 10
    Output: 1024.00000
    Example 2:
    
    Input: x = 2.10000, n = 3
    Output: 9.26100
    Example 3:
    
    Input: x = 2.00000, n = -2
    Output: 0.25000
    Explanation: 2-2 = 1/22 = 1/4 = 0.25

---
+++ Pow(x,n).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;  // handle INT_MIN case
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1;
+        
+        while (N > 0) {
+            if (N % 2 == 1) {   // odd
+                result *= x;
+            }
+            x *= x;   // square base
+            N /= 2;
+        }
+        
+        return result;
+    }
+};
